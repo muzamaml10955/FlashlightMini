5c82fa85d5b9b73844315bd6f1f9c265f0a8eff0
zimtech <71459065+zimtech10955@users.noreply.github.com>
2021-02-18 23:21:06 +0500

    admob banner add fb intestial add added

--- app/src/main/java/com/awsixn/flashlightmini/MainActivity.java
+++ app/src/main/java/com/awsixn/flashlightmini/MainActivity.java
@@ -15,11 +15,20 @@ import androidx.annotation.RequiresApi;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.LinearLayout;
 import android.widget.ToggleButton;
 
 import  com.facebook.ads.*;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.RequestConfiguration;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
+import java.util.Arrays;
 /*
  *
  * its publilish with zimtechjks with muzammal123 passowrd
@@ -38,6 +47,14 @@ public class MainActivity extends Activity {
     boolean flag;
     ToggleButton onoffbutton;
     private AdView adView,adviewtop;
+    private InterstitialAd interstitialAd;
+    String TAG="tag";
+  //  Button add;
+
+
+    // admob init
+    private com.google.android.gms.ads.AdView adViewadmob;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,10 +62,55 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         onoffbutton = findViewById(R.id.tb1);
         Context context = this;
+loadad();
+/*add=findViewById(R.id.add);
+add.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+
+    }
+});*/
 
 
 
-        adView = new AdView(this, "IMG_16_9_APP_INSTALL#YOUR_PLACEMENT_ID", AdSize.RECTANGLE_HEIGHT_250);
+
+
+        // Initialize the Mobile Ads SDK.
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {}
+        });
+
+        // Set your test devices. Check your logcat output for the hashed device ID to
+        // get test ads on a physical device. e.g.
+        // "Use RequestConfiguration.Builder().setTestDeviceIds(Arrays.asList("ABCDEF012345"))
+        // to get test ads on this device."
+        MobileAds.setRequestConfiguration(
+                new RequestConfiguration.Builder().setTestDeviceIds(Arrays.asList("f108046a-9e15-43de-86cf-7f03ac7bd540"))
+                        .build());
+
+        // Gets the ad view defined in layout/ad_fragment.xml with ad unit ID set in
+        // values/strings.xml.
+        adViewadmob = findViewById(R.id.adView_banner_admob);
+
+        // Create an ad request.
+        com.google.android.gms.ads.AdRequest adRequestadmob = new AdRequest.Builder().build();
+
+        // Start loading the ad in the background.
+        adViewadmob.loadAd(new AdRequest.Builder().build());
+//admob ad end here
+
+
+
+
+
+
+
+
+
+
+
+   /*     adView = new AdView(this, getString(R.string.fb_mediumRectr_ad_id), AdSize.RECTANGLE_HEIGHT_250);
 
         // Find the Ad Container
         LinearLayout adContainer = (LinearLayout) findViewById(R.id.banner_container);
@@ -57,12 +119,12 @@ public class MainActivity extends Activity {
         adContainer.addView(adView);
 
         // Request an ad
-        adView.loadAd();
+        adView.loadAd();*/
 
 
 
 
-        adviewtop = new AdView(this, "IMG_16_9_APP_INSTALL#YOUR_PLACEMENT_ID", AdSize.RECTANGLE_HEIGHT_250);
+        adviewtop = new AdView(this, getString(R.string.fb_mediumRectr_ad_id), AdSize.RECTANGLE_HEIGHT_250);
 
         // Find the Ad Container
         LinearLayout adContainertop = (LinearLayout) findViewById(R.id.banner_containertop);
@@ -144,6 +206,7 @@ public class MainActivity extends Activity {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 //called when the button status is changed
+
                 switchFlashLight(isChecked);
             }
         });
@@ -221,6 +284,77 @@ public class MainActivity extends Activity {
 
 
     }
+
+
+    private void loadad() {
+
+
+        // interstitialAd = new InterstitialAd(this, "IMG_16_9_APP_INSTALL#YOUR_PLACEMENT_ID");  //test
+
+        interstitialAd = new InterstitialAd(this, getString(R.string.fb_int_id));
+        // Create listeners for the Interstitial Ad
+        InterstitialAdListener interstitialAdListener = new InterstitialAdListener() {
+            @Override
+            public void onInterstitialDisplayed(Ad ad) {
+                // Interstitial ad displayed callback
+                Log.e(TAG, "Interstitial ad displayed.");
+            }
+
+            @Override
+            public void onInterstitialDismissed(Ad ad) {
+                // Interstitial dismissed callback
+                Log.e(TAG, "Interstitial ad dismissed.");
+            }
+
+            @Override
+            public void onError(Ad ad, AdError adError) {
+                // Ad error callback
+                Log.e(TAG, "Interstitial ad failed to load: " + adError.getErrorMessage());
+            }
+
+            @Override
+            public void onAdLoaded(Ad ad) {
+                // Interstitial ad is loaded and ready to be displayed
+                Log.d(TAG, "Interstitial ad is loaded and ready to be displayed!");
+                // Show the ad
+                interstitialAd.show();
+            }
+
+            @Override
+            public void onAdClicked(Ad ad) {
+                // Ad clicked callback
+                Log.d(TAG, "Interstitial ad clicked!");
+            }
+
+            @Override
+            public void onLoggingImpression(Ad ad) {
+                // Ad impression logged callback
+                Log.d(TAG, "Interstitial ad impression logged!");
+            }
+        };
+
+        // For auto play video ads, it's recommended to load the ad
+        // at least 30 seconds before it is shown
+        interstitialAd.loadAd(
+                interstitialAd.buildLoadAdConfig()
+                        .withAdListener(interstitialAdListener)
+                        .build());
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     @Override
